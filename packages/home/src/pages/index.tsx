f935ae91bc27df1bc84d1bcc75b33927fa3dd714
Shun Kakinoki <shunkakinoki@gmail.com>
2021-11-30 05:30:51 -0500

    perf: ini wagumi dao

--- packages/home/src/pages/index.tsx
+++ packages/home/src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { FooterLogo } from "@/home/components/FooterLogo";
 import { GlowLogo } from "@/home/components/GlowLogo";
 import { DISCORD_URL, GITHUB_URL } from "@/home/const/config";
-import WagumiImage from "@/home/public/wagumi.png";
 
 export const IndexPage = (): JSX.Element => {
   return (
     <div className="w-full min-h-screen bg-black">
       <div className="flex flex-col justify-center items-center h-screen">
-        <GlowLogo src={WagumiImage} />
+        <GlowLogo src="wagumi.png" />
         <FooterLogo discord={DISCORD_URL} github={GITHUB_URL} />
       </div>
     </div>
