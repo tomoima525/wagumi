4afc5b58b23f1d3ae410935ae42435678fbcbded
Shun Kakinoki <shunkakinoki@gmail.com>
2021-11-30 23:16:06 -0500

    chore: update

--- packages/snapshot/src/index.ts
+++ packages/snapshot/src/index.ts
@@ -5,22 +5,24 @@ import { Client, Intents } from "discord.js";
 import "dotenv/config";
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+  ],
 });
 
 export const GUILD_ID = "914960638365810748";
 
 client.login(process.env.DISCORD_BOT_TOKEN);
-
 console.log(process.env.DISCORD_BOT_TOKEN);
 
 client.on("ready", () => {
   let membersList: string[] = [];
-
   let guild = client.guilds.cache.get(GUILD_ID);
-  console.log(guild);
-  console.log(guild?.members.cache);
+
   guild?.members.fetch().then(members => {
+    console.log(members);
     return members.forEach(member => {
       membersList.push(member.user.username);
       return console.log(member.user.username);
