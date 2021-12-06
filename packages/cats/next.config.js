8828deef82c39d0953aba5687046b0d7a142fc77
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-06 17:16:48 -0500

    perf: ini cats rewrites

--- packages/cats/next.config.js
+++ packages/cats/next.config.js
@@ -3,4 +3,12 @@
  */
 module.exports = {
   presets: [require("../../next.config")],
+  async rewrites() {
+    return [
+      {
+        source: "/:slug(\\d{1,})",
+        destination: "/:slug.png",
+      },
+    ];
+  },
 };
