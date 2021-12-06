9101b6dcd4ea36b7c4b1a4a3bfb5883492776299
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-06 17:16:07 -0500

    chore: update

--- packages/cats/src/pages/index.tsx
+++ packages/cats/src/pages/index.tsx
@@ -1,12 +1,10 @@
 import { FooterLogo } from "@/cats/components/FooterLogo";
-import { GlowLogo } from "@/cats/components/GlowLogo";
 import { DISCORD_URL, GITHUB_URL, TWITTER_URL } from "@/cats/const/config";
 
 export const IndexPage = (): JSX.Element => {
   return (
     <div className="w-full min-h-screen bg-black">
       <div className="flex flex-col justify-center items-center h-screen">
-        <GlowLogo src="wagumi.png" />
         <FooterLogo
           discord={DISCORD_URL}
           github={GITHUB_URL}
