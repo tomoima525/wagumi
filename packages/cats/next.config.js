7fab7a9517cb57a661fd20e8c070efae67d3e486
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-09 13:15:06 -0500

    perf: ini slug regex

--- packages/cats/next.config.js
+++ packages/cats/next.config.js
@@ -5,9 +5,16 @@ module.exports = {
   presets: [require("../../next.config")],
   async rewrites() {
     return {
+      beforeFiles: [
+        {
+          source: "/:slug(\\d{1,3})",
+          destination:
+            "https://media.githubusercontent.com/media/wagumi/wagumi/main/generated/:slug.png",
+        },
+      ],
       fallback: [
         {
-          source: "/:slug*",
+          source: "/:slug(\\d{1,3}.(png)",
           destination:
             "https://media.githubusercontent.com/media/wagumi/wagumi/main/generated/:slug*",
         },
