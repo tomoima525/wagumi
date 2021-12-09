9eec4588d8484fb3656e813a357ab4f8a637018a
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-09 10:44:11 -0500

    chore: update

--- packages/cats/next.config.js
+++ packages/cats/next.config.js
@@ -5,13 +5,6 @@ module.exports = {
   presets: [require("../../next.config")],
   async rewrites() {
     return {
-      beforeFiles: [
-        {
-          source: "/:slug(\\d{1,})",
-          destination:
-            "https://media.githubusercontent.com/media/wagumi/wagumi/main/generated/:slug.png",
-        },
-      ],
       fallback: [
         {
           source: "/:slug*",
