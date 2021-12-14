54284329e4637eebb2da8e5347da15c6ed4ac28c
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-13 21:23:03 -0800

    perf: ini deploy script

--- packages/contracts/deploy/WagumiCats.ts
+++ packages/contracts/deploy/WagumiCats.ts
@@ -1,21 +1,20 @@
 import type { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const deploy = async ({
-  getNamedAccounts,
-  deployments,
-  network,
-}: HardhatRuntimeEnvironment) => {
+import type { WagumiCats } from "@/typechain/WagumiCats";
+import type { WagumiCats__factory } from "@/typechain/factories/WagumiCats__factory";
+
+const deploy = async ({ ethers, network }: HardhatRuntimeEnvironment) => {
   if (network.name !== "hardhat") {
     return;
   }
 
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
-  await deploy("NFT", {
-    args: ["https://wagumi.xyz/art/metadata/"],
-    from: deployer,
-    log: true,
-  });
+  const greeterFactory = (await ethers.getContractFactory(
+    "Greeter",
+  )) as WagumiCats__factory;
+  const greeter: WagumiCats = await greeterFactory.deploy(
+    "https://cats.wagumi.xyz/metadata/",
+  );
+  await greeter.deployed();
 };
 
 deploy.tags = ["WagumiCats"];
