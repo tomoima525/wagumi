3150f4b3cf61d7c595922a4fc2740ce8ab68d602
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-13 20:26:35 -0800

    perf: ini discord bot

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -1,5 +1,3 @@
-import { writeFile } from "fs";
-
 import { Client } from "discord.js";
 
 import "dotenv/config";
@@ -9,31 +7,12 @@ const client = new Client();
 export const GUILD_ID = "914960638365810748";
 
 client.login(process.env.DISCORD_BOT_TOKEN);
-console.log(process.env.DISCORD_BOT_TOKEN);
-
-client.on("ready", () => {
-  let membersList: string[] = [];
-  let guild = client.guilds.cache.get(GUILD_ID);
-
-  guild?.members.fetch().then(members => {
-    console.log(members);
-    return members.forEach(member => {
-      membersList.push(member.user.username);
-      return console.log(member.user.username);
-    });
-  });
-  writeFile("snapshot.json", JSON.stringify(membersList), err => {
-    if (err) {
-      return console.error(err);
-    }
-    console.log("File created!");
-  });
-});
 
 client.on("message", async message => {
-  // When the user sends !ping the bot sends Pong.
-  if (message.content === "!ping") {
-    // メッセージが送信されたチャンネルへ「Pong.」を送り返す。
-    message.channel.send("Pong.");
+  // development-test channel
+  if (message.channel.id == "920167819360993330") {
+    if (message.content === "!ping") {
+      message.channel.send("Pong.");
+    }
   }
 });
