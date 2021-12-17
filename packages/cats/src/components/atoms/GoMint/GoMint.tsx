818108b70e7cbfc6aaaf1e14b6c8ba34255346c3
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-17 14:17:01 -0800

    chore: update

--- packages/cats/src/components/atoms/GoMint/GoMint.tsx
+++ packages/cats/src/components/atoms/GoMint/GoMint.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export const GoMint = () => {
   return (
-    <Link href="/mint">
-      <a className="p-1 mt-2 w-full max-w-xs text-2xl text-center text-white hover:bg-slate-300 hover:bg-opacity-30 rounded-md border border-current">
-        Mint
-      </a>
-    </Link>
+    <div className="flex w-full max-w-xs">
+      <Link href="/mint">
+        <a className="p-1 mt-2 w-full text-2xl text-center text-white hover:bg-gray-300 hover:bg-opacity-30 rounded-md border border-current">
+          Mint
+        </a>
+      </Link>
+    </div>
   );
 };
