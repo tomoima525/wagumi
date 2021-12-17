6c6c13abeb667b9eafe117774524718b72f97a5c
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-17 13:46:29 -0800

    chore: update

--- packages/cats/src/components/templates/FooterLogo/FooterLogo.tsx
+++ packages/cats/src/components/templates/FooterLogo/FooterLogo.tsx
@@ -36,19 +36,19 @@ export const FooterLogo: FC<FooterLogoProps> = ({
         {discord && (
           <FooterIconLink href={discord}>
             <span className="sr-only">Discord</span>
-            <FaDiscord className="w-6 h-6" />
+            <FaDiscord className="w-8 h-8" />
           </FooterIconLink>
         )}
         {github && (
           <FooterIconLink href={github}>
             <span className="sr-only">Github</span>
-            <FaGithub className="w-6 h-6" />
+            <FaGithub className="w-8 h-8" />
           </FooterIconLink>
         )}
         {twitter && (
           <FooterIconLink href={twitter}>
             <span className="sr-only">Twitter</span>
-            <FaTwitter className="w-6 h-6" />
+            <FaTwitter className="w-8 h-8" />
           </FooterIconLink>
         )}
       </div>
