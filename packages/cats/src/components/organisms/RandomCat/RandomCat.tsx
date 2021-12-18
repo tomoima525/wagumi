7d87b229c338a7f0da26474d3559f023758f2015
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-18 10:47:46 -0800

    chore: update

--- packages/cats/src/components/organisms/RandomCat/RandomCat.tsx
+++ packages/cats/src/components/organisms/RandomCat/RandomCat.tsx
@@ -61,7 +61,7 @@ export const RandomCat: FC = () => {
           Generate Random Wagumi Cat
         </button>
       </div>
-      <div className="flex py-3 w-full max-w-xs">
+      <div className="flex py-4 w-full max-w-xs">
         <Link href="/mint">
           <a className="p-1 mt-2 w-full text-2xl text-center text-white hover:bg-gray-300 hover:bg-opacity-30 rounded-md border border-current">
             Go Mint
