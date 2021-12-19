4799bf424ae07fb4920147e7b9aba3a7f17d357e
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-18 22:34:46 -0800

    chore: update

--- packages/contracts/deploy/Check.ts
+++ packages/contracts/deploy/Check.ts
@@ -3,9 +3,13 @@ import type { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const check: DeployFunction = async ({
   getNamedAccounts,
+  ethers,
 }: HardhatRuntimeEnvironment) => {
   const { deployer } = await getNamedAccounts();
   console.log("Deploying contracts with the account:", deployer);
+  const provider = ethers.getDefaultProvider();
+  const balance = await provider.getBalance(deployer);
+  console.log("Deploying account has the balance:", balance);
 };
 
 check.tags = ["Check"];
