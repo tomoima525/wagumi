985935d6249a7539cf1a5b13ba160484600aaf4a
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-19 12:28:37 -0800

    fix: revamp wallet provider

--- packages/cats/src/pages/_app.tsx
+++ packages/cats/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from "next/app";
 import { Suspense } from "react";
 
 import { ErrorBoundary } from "react-error-boundary";
+import Web3Modal from "web3modal";
 
 import "@/cats/styles/index.css";
 import { Error } from "@/cats/components/atoms/Error";
@@ -14,22 +15,30 @@ import { Intro } from "@/cats/components/templates/Intro";
 import { Seo } from "@/cats/components/templates/Seo";
 import { SwitchNetwork } from "@/cats/components/templates/SwitchNetwork";
 
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
+    },
+  },
+};
+
+const web3Modal = new Web3Modal({
+  network: "mainnet",
+  cacheProvider: true,
+  providerOptions: providerOptions,
+});
+
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <Container>
       <Seo />
       <WalletProvider
         cacheProvider
-        providerOptions={{
-          walletconnect: {
-            package: WalletConnectProvider,
-            options: {
-              infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
-            },
-          },
-        }}
         fallback={<Intro />}
         loading={<Loading />}
+        web3Modal={web3Modal}
       >
         <ErrorBoundary FallbackComponent={Error}>
           <Suspense fallback={<Loading />}>
