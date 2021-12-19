2ef4a2353bdce07cc9ec45b4cb46eae2969a2e8b
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-18 23:07:01 -0800

    chore: update

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,5 +1,3 @@
 export { default as WagumiCatsABI } from "@/abi/contracts/WagumiCats.sol/WagumiCats.json";
 export { default as WagumiCatsDeployment } from "@/deployments/mainnet/WagumiCats.json";
-export { default as WagumiCatsMainnetDeployment } from "@/deployments/mainnet/WagumiCats.json";
-export { default as WagumiCatsRinkebyDeployment } from "@/deployments/rinkeby/WagumiCats.json";
 export { WagumiCats__factory } from "@/typechain/factories/WagumiCats__factory";
