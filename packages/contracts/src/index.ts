26b8546fe6596757408bc3cd4c4b930b8c0dedc9
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-18 17:16:23 -0800

    perf: ini wagumi typechain

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,3 +1,3 @@
 export { default as WagumiCatsABI } from "@/abi/contracts/WagumiCats.sol/WagumiCats.json";
 export { default as WagumiCatsRinkebyDeployment } from "@/deployments/rinkeby/WagumiCats.json";
-export type { WagumiCats__factory } from "@/typechain/factories/WagumiCats__factory";
+export { WagumiCats__factory } from "@/typechain/factories/WagumiCats__factory";
